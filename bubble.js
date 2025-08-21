b91ecbac3808948aeccb1e4b89fce4afd5ade84c
zurabdzhugashvil <zurabdzhugashvili@gmail.com>
2025-08-21 05:27:29 +0200

    Add files via upload

---
+++ bubble.js
@@ -0,0 +1,11 @@
+function bubbleSortConcept1(arr) {
+  for (let j = arr.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
+    }
+  }
+}
